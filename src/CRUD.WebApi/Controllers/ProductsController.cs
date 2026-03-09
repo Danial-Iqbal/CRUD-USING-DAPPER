515b6791f9a7e898f9e0f8fde4ce040045d9fff4
DANIAL IQBAL <expertdanial@gmail.com>
2026-03-09 23:22:50 +0400

    Add Product Controller

---
+++ src/CRUD.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using CRUD.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUD.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductsController : BaseController
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> GetAll()
+        {
+            var products = await _productRepository.GetAllAsync();
+
+            return Ok(products);
+        }
+    }
+}
