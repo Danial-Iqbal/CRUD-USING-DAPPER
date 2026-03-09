61f13d2a3a330cc49eee08b0b42018e2576a75f9
DANIAL IQBAL <expertdanial@gmail.com>
2026-03-09 23:09:50 +0400

    ProductRepository => GetAllAsync

--- src/CRUD.WebApi/Repositories/ProductRepository.cs
+++ src/CRUD.WebApi/Repositories/ProductRepository.cs
@@ -1,9 +1,17 @@
-﻿using CRUD.WebApi.Models;
+﻿using CRUD.WebApi.Data;
+using CRUD.WebApi.Models;
+using Dapper;
 
 namespace CRUD.WebApi.Repositories
 {
     public class ProductRepository : IProductRepository
     {
+        private readonly DapperContext _dbContext;
+
+        public ProductRepository(DapperContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
         public Task<int> CreateAsync(Product product)
         {
             throw new NotImplementedException();
@@ -16,7 +24,19 @@ namespace CRUD.WebApi.Repositories
 
         public Task<IEnumerable<Product>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            var query = @"SELECT Id,
+                                 Name,
+                                 Description, 
+                                 Price     
+                          FROM Products
+                          ORDER BY Id DESC
+                         ";
+
+            using var connection = _dbContext.CreateConnection();
+
+            var products = connection.QueryAsync<Product>(query);
+
+            return products;
         }
 
         public Task<Product> GetByIdAsync(int id)
